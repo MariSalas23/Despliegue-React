f16ddbce3e10ab804a325dbc57b8931d9e5509d3
MariSalas23 <mari.masagu@gmail.com>
2024-04-04 20:59:30 -0500

    Update Design

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       if (e.target.classList.contains('character')) {
-        handleSubmitCharacter(e);
+        searchCharacter();
       } else if (e.target.classList.contains('phrase')) {
         handleSubmitPhrase(e);
       } else if (e.target.classList.contains('cont')) {
@@ -21,81 +21,20 @@ function App() {
     }
   };
 
-  const handleSubmitCharacter = async (e) => {
-    e.preventDefault();
-    if (characterName.trim() !== '') {
-      try {
-        const response = await fetch(
-          `https://thesimpsonsquoteapi.glitch.me/quotes?character=${encodeURIComponent(
-            characterName
-          )}`
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch character data');
-        }
-        const data = await response.json();
-        if (data.length === 0) {
-          throw new Error('No character data found');
-        }
-        setCharacter(data[0]);
-      } catch (error) {
-        console.error('Error fetching character data:', error);
-        alert('Error fetching character data. Please try again later.');
+  const searchCharacter = async () => {
+    try {
+      const response = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${encodeURIComponent(characterName)}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch character data');
       }
-    } else {
-      alert('Please enter a character name.');
-    }
-  };
-
-  const handleSubmitPhrase = async (e) => {
-    e.preventDefault();
-    const phrase = e.target.value.trim();
-    if (phrase !== '') {
-      try {
-        const response = await fetch(
-          `https://thesimpsonsquoteapi.glitch.me/quotes`
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch quote data');
-        }
-        const data = await response.json();
-        const matchingQuote = data.find((quote) =>
-          quote.quote.toLowerCase().includes(phrase.toLowerCase())
-        );
-        if (!matchingQuote) {
-          throw new Error('No quote data found');
-        }
-        setCharacter(matchingQuote);
-      } catch (error) {
-        console.error('Error fetching quote data:', error);
-        alert('Error fetching quote data. Please try again later.');
+      const data = await response.json();
+      if (data.length === 0) {
+        throw new Error('No character data found');
       }
-    } else {
-      alert('Please enter a phrase.');
-    }
-  };
-
-  const handleSubmitCount = async (e) => {
-    e.preventDefault();
-    const count = parseInt(e.target.value);
-    if (count >= 1 && count <= 3) {
-      try {
-        const response = await fetch(
-          `https://thesimpsonsquoteapi.glitch.me/quotes?character=${encodeURIComponent(
-            characterName
-          )}&count=${count}`
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch quote data for character');
-        }
-        const data = await response.json();
-        setCharacter(data[0]);
-      } catch (error) {
-        console.error('Error fetching quote data for character:', error);
-        alert('Error fetching quote data for character. Please try again later.');
-      }
-    } else {
-      alert('Please enter a number between 1 and 3.');
+      setCharacter(data[0]);
+    } catch (error) {
+      console.error('Error fetching character data:', error);
+      alert('Error fetching character data. Please try again later.');
     }
   };
 
@@ -107,20 +46,21 @@ function App() {
       
       <div className="blue-container">
         <div className="section">
+
           <form>
             <div className="form-row">
                 <label htmlFor="character">Name:</label>
-                <input type="text" className="character" placeholder="Enter the character" required onKeyPress={handleKeyPress} />
+                <input type="text" className="character" placeholder="Enter the character" onKeyPress={handleKeyPress} />
             </div>
             
             <div className="form-row">
                 <label htmlFor="phrase">Quote:</label>
-                <input type="text" className="phrase" placeholder="Enter a quote" required onKeyPress={handleKeyPress} />
+                <input type="text" className="phrase" placeholder="Enter a quote" onKeyPress={handleKeyPress} />
             </div>
             
             <div className="form-row">
                 <label htmlFor="cont">Number of phrases:</label>
-                <input type="text" className="cont" placeholder="Enter a number" required onKeyPress={handleKeyPress} />
+                <input type="text" className="cont" placeholder="Enter a number" onKeyPress={handleKeyPress} />
             </div>
 
             <div className="form-row">
@@ -129,7 +69,15 @@ function App() {
             </div>
           </form>
 
-          <div className="character-container"> </div>
+          <div className="character-container">
+            {character && (
+              <Card
+                name={character.character}
+                image={character.image}
+                quote={character.quote}
+              />
+            )}
+          </div>
         </div>
       </div>
 
--- my-react-app/src/Card/Card.jsx
+++ my-react-app/src/Card/Card.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-function Card({ character }) {
-  return (
-    <div className="card">
-      <img src={character.image} alt="Character" />
-      <div>
-        <h2>Character: {character.character}</h2>
-        <h3>Quotes:</h3>
-        <ul>
-          {character.quotes.map((quote, index) => (
-            <li key={index}>{quote}</li>
-          ))}
-        </ul>
+function Card({ name, image, quote }) {
+    return (
+      <div className="card">
+        <div className="image-container-character">
+           <img src={image} alt="Character" /> 
+        </div>
+        
+        <div className="text-container-character">
+          <h2>Character:</h2>
+          <p>{name}</p>
+          <h2>Quote:</h2>
+          <p>{quote}</p>
+        </div>
       </div>
-    </div>
-  );
-}
-
-export default Card;
+    );
+  }
+  
+  export default Card;
